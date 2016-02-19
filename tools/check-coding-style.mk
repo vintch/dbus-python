bfa02dd9f85f90200de796ad2d9e65596e8add6b
Simon McVittie <simon.mcvittie@collabora.co.uk>
2016-02-19 12:17:26 +0000

    check-coding-style: work in out-of-tree builds

--- tools/check-coding-style.mk
+++ tools/check-coding-style.mk
@@ -3,20 +3,20 @@ check-local::
 	cd $(srcdir) || exit $$?; \
 	if test -n "$(check_misc_sources)"; then \
 		echo check-coding-style.mk: checking misc sources...; \
-		top_srcdir=$(top_srcdir) \
-		sh $(top_srcdir)/tools/check-whitespace.sh \
+		top_srcdir=$(abs_top_srcdir) \
+		sh $(abs_top_srcdir)/tools/check-whitespace.sh \
 			$(check_misc_sources) || fail=1; \
 	fi; \
 	if test -n "$(check_py_sources)"; then \
 		echo check-coding-style.mk: checking Python sources...; \
-		top_srcdir=$(top_srcdir) \
-		sh $(top_srcdir)/tools/check-py-style.sh \
+		top_srcdir=$(abs_top_srcdir) \
+		sh $(abs_top_srcdir)/tools/check-py-style.sh \
 			$(check_py_sources) || fail=1; \
 	fi;\
 	if test -n "$(check_c_sources)"; then \
 		echo check-coding-style.mk: checking C sources...; \
-		top_srcdir=$(top_srcdir) \
-		sh $(top_srcdir)/tools/check-c-style.sh \
+		top_srcdir=$(abs_top_srcdir) \
+		sh $(abs_top_srcdir)/tools/check-c-style.sh \
 			$(check_c_sources) || fail=1; \
 	fi;\
 	if test yes = "@ENABLE_CODING_STYLE_CHECKS@"; then \
