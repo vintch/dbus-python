1013581fef91c9be087d6264084cec34c2ca6e9a
Simon McVittie <smcv@debian.org>
2016-08-11 09:58:58 +0100

    test/import-repeatedly: skip on Python 3.6 for now
    This is a workaround for <http://bugs.python.org/issue27736>.
    
    Signed-off-by: Simon McVittie <smcv@debian.org>

--- test/import-repeatedly.c
+++ test/import-repeatedly.c
@@ -1,6 +1,7 @@
 /* Regression test for https://bugs.freedesktop.org/show_bug.cgi?id=23831 */
 
 #include <stdio.h>
+#include <string.h>
 
 #include <Python.h>
 
@@ -12,10 +13,20 @@ int main(void)
 
     for (i = 0; i < 100; ++i) {
         Py_Initialize();
+
         if (PyRun_SimpleString("import dbus\n") != 0) {
             puts("not ok 1 - there was an exception");
             return 1;
         }
+
+        /* This is known not to work in Python 3.6.0a3, for reasons that
+         * are not a dbus-python bug. */
+        if (strcmp(Py_GetVersion(), "3.6") >= 0) {
+            puts("ok 1 # SKIP - http://bugs.python.org/issue27736");
+            Py_Finalize();
+            return 0;
+        }
+
         Py_Finalize();
     }
 
