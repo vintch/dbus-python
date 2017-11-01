7108baab93b6edd16343a58af7f76638ad11f758
Simon McVittie <smcv@collabora.com>
2017-11-01 16:46:57 +0000

    Revert "test/import-repeatedly: skip on Python 3.6 for now"
    Python 3.6.0a4 is meant to have fixed this.
    
    This reverts commit 1013581fef91c9be087d6264084cec34c2ca6e9a.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- test/import-repeatedly.c
+++ test/import-repeatedly.c
@@ -1,7 +1,6 @@
 /* Regression test for https://bugs.freedesktop.org/show_bug.cgi?id=23831 */
 
 #include <stdio.h>
-#include <string.h>
 
 #include <Python.h>
 
@@ -13,20 +12,10 @@ int main(void)
 
     for (i = 0; i < 100; ++i) {
         Py_Initialize();
-
         if (PyRun_SimpleString("import dbus\n") != 0) {
             puts("not ok 1 - there was an exception");
             return 1;
         }
-
-        /* This is known not to work in Python 3.6.0a3, for reasons that
-         * are not a dbus-python bug. */
-        if (strcmp(Py_GetVersion(), "3.6") >= 0) {
-            puts("ok 1 # SKIP - http://bugs.python.org/issue27736");
-            Py_Finalize();
-            return 0;
-        }
-
         Py_Finalize();
     }
 
